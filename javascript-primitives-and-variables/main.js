388aecffe7c1263cbc120165a2df5580949bad94
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-06-28 10:34:34 -0700

    javascript-primitives-and-variables: update1

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,24 @@
+var fullName = 'Ryan Skidmore';
+
+var isCool = false;
+
+var totalPets = 100;
+
+console.log('Name ', fullName);
+console.log('Is Cool? ', isCool);
+console.log('How many pets? ', totalPets);
+
+console.log('Fullname typeof: ', typeof fullName);
+console.log('isCool typeof: ', typeof isCool);
+console.log('totalPets typeof: ', typeof totalPets);
+
+var unknown = null;
+
+console.log('Unknown: ', unknown);
+console.log('Unknown typeof: ', typeof unknown);
+
+var never;
+
+console.log('Never var: ', never);
+
+console.log('Never typeof: ', never);
