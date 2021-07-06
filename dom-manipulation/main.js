f01acf3ce6b83e7ba6bf17a145e05a983642cd82
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-07-06 16:41:35 -0700

    dom-manipulation: hotBtn applied in if else statement

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -7,23 +7,18 @@ hotBtn.addEventListener('click', function (event) {
   count += 1;
   clickCount.textContent = 'Clicks: ' + count;
   if (count === 4) {
-    var temp = document.querySelector('.hot-button.cold');
-    temp.className = 'hot-button cool';
+    hotBtn.className = 'hot-button cool';
   } else if (count === 7) {
-    temp = document.querySelector('.hot-button.cool');
-    temp.className = 'hot-button tepid';
+    hotBtn.className = 'hot-button tepid';
 
   } else if (count === 10) {
-    temp = document.querySelector('.hot-button.tepid');
-    temp.className = 'hot-button warm';
+    hotBtn.className = 'hot-button warm';
 
   } else if (count === 13) {
-    temp = document.querySelector('.hot-button.warm');
-    temp.className = 'hot-button hot';
+    hotBtn.className = 'hot-button hot';
 
   } else if (count === 16) {
-    temp = document.querySelector('.hot-button.hot');
-    temp.className = 'hot-button nuclear';
+    hotBtn.className = 'hot-button nuclear';
 
   }
 });
