8d43c4f442fe5a564a950e67a4bca0b647807559
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-07-11 23:32:00 -0700

    string-manipulation-3: store

---
+++ string-manipulation-3/src/title-case.js
@@ -0,0 +1,10 @@
+/* exported titleCase */
+
+// split arg title by ' ' into array
+// Enter loop over array
+// Switch statement for excluded words
+// included spcial exceptions
+
+function titleCase(title) {
+
+}
