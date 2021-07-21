751973e3df5948270b58756f4f997a958e1fc2a9
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-07-21 16:00:36 -0700

    Auto-generate deck

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -19,216 +19,232 @@ var players = [
   }
 ];
 
-var deck = [
-  {
-    suit: 'hearts',
-    rank: 2
-  },
-  {
-    suit: 'hearts',
-    rank: 3
-  },
-  {
-    suit: 'hearts',
-    rank: 4
-  },
-  {
-    suit: 'hearts',
-    rank: 5
-  },
-  {
-    suit: 'hearts',
-    rank: 6
-  },
-  {
-    suit: 'hearts',
-    rank: 7
-  },
-  {
-    suit: 'hearts',
-    rank: 8
-  },
-  {
-    suit: 'hearts',
-    rank: 9
-  },
-  {
-    suit: 'hearts',
-    rank: 10
-  },
-  {
-    suit: 'hearts',
-    rank: 'J'
-  },
-  {
-    suit: 'hearts',
-    rank: 'Q'
-  },
-  {
-    suit: 'hearts',
-    rank: 'K'
-  },
-  {
-    suit: 'hearts',
-    rank: 'A'
-  },
-  {
-    suit: 'diamonds',
-    rank: 2
-  },
-  {
-    suit: 'diamonds',
-    rank: 3
-  },
-  {
-    suit: 'diamonds',
-    rank: 4
-  },
-  {
-    suit: 'diamonds',
-    rank: 5
-  },
-  {
-    suit: 'diamonds',
-    rank: 6
-  },
-  {
-    suit: 'diamonds',
-    rank: 7
-  },
-  {
-    suit: 'diamonds',
-    rank: 8
-  },
-  {
-    suit: 'diamonds',
-    rank: 9
-  },
-  {
-    suit: 'diamonds',
-    rank: 10
-  },
-  {
-    suit: 'diamonds',
-    rank: 'J'
-  },
-  {
-    suit: 'diamonds',
-    rank: 'Q'
-  },
-  {
-    suit: 'diamonds',
-    rank: 'K'
-  },
-  {
-    suit: 'diamonds',
-    rank: 'A'
-  },
-  {
-    suit: 'clubs',
-    rank: 2
-  },
-  {
-    suit: 'clubs',
-    rank: 3
-  },
-  {
-    suit: 'clubs',
-    rank: 4
-  },
-  {
-    suit: 'clubs',
-    rank: 5
-  },
-  {
-    suit: 'clubs',
-    rank: 6
-  },
-  {
-    suit: 'clubs',
-    rank: 7
-  },
-  {
-    suit: 'clubs',
-    rank: 8
-  },
-  {
-    suit: 'clubs',
-    rank: 9
-  },
-  {
-    suit: 'clubs',
-    rank: 10
-  },
-  {
-    suit: 'clubs',
-    rank: 'J'
-  },
-  {
-    suit: 'clubs',
-    rank: 'Q'
-  },
-  {
-    suit: 'clubs',
-    rank: 'K'
-  },
-  {
-    suit: 'clubs',
-    rank: 'A'
-  },
-  {
-    suit: 'spades',
-    rank: 2
-  },
-  {
-    suit: 'spades',
-    rank: 3
-  },
-  {
-    suit: 'spades',
-    rank: 4
-  },
-  {
-    suit: 'spades',
-    rank: 5
-  },
-  {
-    suit: 'spades',
-    rank: 6
-  },
-  {
-    suit: 'spades',
-    rank: 7
-  },
-  {
-    suit: 'spades',
-    rank: 8
-  },
-  {
-    suit: 'spades',
-    rank: 9
-  },
-  {
-    suit: 'spades',
-    rank: 10
-  },
-  {
-    suit: 'spades',
-    rank: 'J'
-  },
-  {
-    suit: 'spades',
-    rank: 'Q'
-  },
-  {
-    suit: 'spades',
-    rank: 'K'
-  },
-  {
-    suit: 'spades',
-    rank: 'A'
+// var deck = [
+//   {
+//     suit: 'hearts',
+//     rank: 2
+//   },
+//   {
+//     suit: 'hearts',
+//     rank: 3
+//   },
+//   {
+//     suit: 'hearts',
+//     rank: 4
+//   },
+//   {
+//     suit: 'hearts',
+//     rank: 5
+//   },
+//   {
+//     suit: 'hearts',
+//     rank: 6
+//   },
+//   {
+//     suit: 'hearts',
+//     rank: 7
+//   },
+//   {
+//     suit: 'hearts',
+//     rank: 8
+//   },
+//   {
+//     suit: 'hearts',
+//     rank: 9
+//   },
+//   {
+//     suit: 'hearts',
+//     rank: 10
+//   },
+//   {
+//     suit: 'hearts',
+//     rank: 'J'
+//   },
+//   {
+//     suit: 'hearts',
+//     rank: 'Q'
+//   },
+//   {
+//     suit: 'hearts',
+//     rank: 'K'
+//   },
+//   {
+//     suit: 'hearts',
+//     rank: 'A'
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 2
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 3
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 4
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 5
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 6
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 7
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 8
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 9
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 10
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 'J'
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 'Q'
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 'K'
+//   },
+//   {
+//     suit: 'diamonds',
+//     rank: 'A'
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 2
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 3
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 4
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 5
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 6
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 7
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 8
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 9
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 10
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 'J'
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 'Q'
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 'K'
+//   },
+//   {
+//     suit: 'clubs',
+//     rank: 'A'
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 2
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 3
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 4
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 5
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 6
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 7
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 8
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 9
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 10
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 'J'
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 'Q'
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 'K'
+//   },
+//   {
+//     suit: 'spades',
+//     rank: 'A'
+//   }
+// ];
+
+var suit = ['hearts', 'diamonds', 'clubs', 'spades'];
+
+var rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+function generateDeck(rank, suit) {
+  var deck = [];
+
+  for (var i = 0; i < suit.length; i++) {
+    var currentSuit = suit[i];
+    for (var j = 0; j < rank.length; j++) {
+      deck.push({ suit: currentSuit, rank: rank[j] });
+    }
   }
-];
+  return deck;
+}
+
 function shuffle(deckInput) {
   return deckInput.sort(() => Math.random() - 0.5);
 }
@@ -285,6 +301,6 @@ function determineWinner(playersInput) {
 
 }
 
-var gameDeck = shuffle(deck);
+// var gameDeck = shuffle(deck);
 
-determineWinner(dealCards(gameDeck, players));
+determineWinner(dealCards(generateDeck(rank, suit), players));
