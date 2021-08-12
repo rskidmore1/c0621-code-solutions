00fcfe22260a44fdd801b930804390086d6e5bd9
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-08-12 20:09:11 +0000

    node-notes-cli: it works

---
+++ node-notes-cli/notes.js
@@ -0,0 +1,111 @@
+//-create read d
+//-create add d
+//-create del d
+//-crete update
+//-create make sure eveyrthing has a return request
+  //-pass things to file write
+//-clean things up
+//-put them into their own files
+
+const fs = require('fs');
+const jsonFile = require('./data');
+
+if (process.argv[2] === 'read'){
+  console.log(read());
+} else if (process.argv[2] === 'add') {
+  add(process.argv[3]);
+} else if (process.argv[2] === 'delete'){
+  del(process.argv[3])
+} else if (process.argv[2] === 'update') {
+  update(process.argv[3], process.argv[4])
+}
+
+
+function read(){
+  let data = jsonFile.notes;
+  let output = '';
+
+  for (var i = 0; i < Object.keys(data).length; i++) {
+    // console.log(`${Object.keys(jsonFile.notes)[i]}: ${Object.values(jsonFile.notes)[i]}`)
+    output += `${Object.keys(data)[i]}: ${Object.values(data)[i]} \n`
+  }
+  return output;
+}
+
+
+//read data file
+//append input to data
+//write that data to file
+function add(input){
+  // let data = jsonFile.notes;
+
+  jsonFile.notes[jsonFile.nextId] = input;
+  jsonFile.nextId++;
+
+  jsonFileStr = JSON.stringify(jsonFile, null, 2);
+
+  fs.writeFile('data.json', jsonFileStr, (err)=> {
+    if (err) throw err;
+
+
+
+  })
+}
+
+//read id to remove
+//get index of that Id
+// splice at that index NO
+//remove with loop
+//try remove with destructuring
+
+//rewrite data. Maybe use add or make a write file function
+function del(input){
+  // let index = Object.keys(jsonFile.notes).indexOf(input)
+  // console.log(index)
+
+  // console.log(Object.keys(jsonFile.notes).splice(index, 1))
+  let newNotes = {}
+  for (var i = 0; i < Object.keys(jsonFile.notes).length; i++){
+    if (Object.keys(jsonFile.notes)[i] !== input ){
+      // console.log('this is not the key')
+      newNotes[Object.keys(jsonFile.notes)[i]] = Object.values(jsonFile.notes)[i]
+    }
+  }
+  // console.log(newNotes);
+  jsonFile.notes = newNotes;
+  jsonFileStr = JSON.stringify(jsonFile, null, 2);
+
+  fs.writeFile('data.json', jsonFileStr, (err) => {
+    if (err) throw err;
+
+
+
+  })
+
+}
+
+
+function update(key, input) {
+  // let index = Object.keys(jsonFile.notes).indexOf(input)
+  // console.log(index)
+
+  // console.log(Object.keys(jsonFile.notes).splice(index, 1))
+  // let newNotes = {}
+  for (var i = 0; i < Object.keys(jsonFile.notes).length; i++) {
+    if (Object.keys(jsonFile.notes)[i] === key) {
+      // console.log('this is not the key')
+      jsonFile.notes[Object.keys(jsonFile.notes)[i]] = input;
+    }
+  }
+  // console.log(newNotes);
+  // jsonFile.notes = newNotes
+  jsonFileStr = JSON.stringify(jsonFile, null, 2);
+
+  fs.writeFile('data.json', jsonFileStr, (err) => {
+    if (err) throw err;
+
+
+
+  })
+
+}
