0599f4121817dfb2d02441929935eef07efc35c0
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-08-12 22:51:01 +0000

    Lops removed

--- node-notes-cli/delete.js
+++ node-notes-cli/delete.js
@@ -1,14 +1,8 @@
 const jsonFile = require('./data');
 
 function del(input) {
+  delete jsonFile.notes[input];
 
-  let newNotes = {}
-  for (var i = 0; i < Object.keys(jsonFile.notes).length; i++) {
-    if (Object.keys(jsonFile.notes)[i] !== input) {
-      newNotes[Object.keys(jsonFile.notes)[i]] = Object.values(jsonFile.notes)[i]
-    }
-  }
-  jsonFile.notes = newNotes;
   return jsonFile;
 }
 
--- node-notes-cli/update.js
+++ node-notes-cli/update.js
@@ -1,12 +1,8 @@
 const jsonFile = require('./data');
 
 function update(key, input) {
+  jsonFile.notes[key] = input;
 
-  for (var i = 0; i < Object.keys(jsonFile.notes).length; i++) {
-    if (Object.keys(jsonFile.notes)[i] === key) {
-      jsonFile.notes[Object.keys(jsonFile.notes)[i]] = input;
-    }
-  }
   return jsonFile;
 }
 
