0dce36c61c394580c9ab787ab6466cc9a2640068
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-08-16 20:32:26 +0000

    express-delete: update1

---
+++ express-delete/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+let app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+}
+
+
+app.get('/api/grades', (req,res) => {
+  let gradeSend = []
+  for (let grade in grades) {
+    gradeSend.push(grades[grade]);
+  }
+  res.json(gradeSend);
+})
+
+
+app.delete('/api/grades/:id', (req, res) => {
+  delete grades[req.params.id];
+  res.sendStatus(204);
+})
+
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+})
