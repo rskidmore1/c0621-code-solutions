a39de3bc8c5d2e416407e561a8296031f67b1e53
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-08-24 00:01:11 +0000

    update1

---
+++ react-elements/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const elem = React.createElement('h1', null, 'Hello, React!');
+
+ReactDOM.render(elem, document.getElementById('root'));
