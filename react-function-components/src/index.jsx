c62d76c6e338ce7424045c64654a74f9b7b222e0
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-08-24 18:55:08 +0000

    Completed function components

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const CustomButton = <button>Click Me!</button>;
+
+ReactDOM.render(
+  CustomButton,
+  document.querySelector('#root')
+);
