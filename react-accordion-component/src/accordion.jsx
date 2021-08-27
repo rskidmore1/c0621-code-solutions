e2ea651fd812efd61225307bee07be12537f880f
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-08-27 02:57:52 +0000

    Object content showing

---
+++ react-accordion-component/src/accordion.jsx
@@ -0,0 +1,37 @@
+// import React from 'react';
+
+// function TestComp(props) {
+
+//   return (
+//     <p>{props}</p>
+//   );
+// }
+
+// function click() {
+//   return true;
+// }
+
+// function Accordian(props) {
+//   const subBoxes = (
+//     <div>
+//       {props.subjects.map(lang =>
+//         <div key={lang.lang}>
+//           {lang.lang}
+//         </div>
+//       )}
+//     </div>
+
+//   );
+
+//   return (
+//     <div >
+//       {subBoxes}
+//       {/* <TestComp /> */}
+//     </div>
+//   );
+// }
+// export default Accordian;
+
+// class
+
+// export default Accordian;
