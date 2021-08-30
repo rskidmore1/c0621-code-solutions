344d212719fc7beea08400a214a7e5f9d549529e
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-08-30 18:05:17 +0000

    Completed express static

---
+++ express-static/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const pathName = path.join(__dirname, 'public');
+
+console.log(pathName);
+
+// const staticVar = express.static(pathName);
+
+const mountUse = app.use(express.static(pathName));
+
+app.get('/', function (req, res) {
+  res.send(mountUse);
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
