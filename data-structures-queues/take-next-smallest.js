63a8cd36007261d5220c9d2c0b9ae7437f32d97a
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-09-22 19:31:45 +0000

    Completed data struct queues

--- data-structures-queues/take-next-smallest.js
+++ data-structures-queues/take-next-smallest.js
@@ -1,48 +1,6 @@
 /* exported takeNextSmallest */
 
 function takeNextSmallest(queue) {
-  // console.log(queue.print());
-
-  // if (queue.peek() !== undefined) {
-  //   // const first = queue.dequeue();
-  //   const arr = [];
-  //   while ((queue.peek() !== undefined)) {
-  //     // console.log(queue.peek());
-  //     arr.push(queue.dequeue());
-  //   }
-  //   // console.log(arr);
-  //   const smallest = arr.shift();
-  //   for (let i = 0; i < arr.length; i++) {
-  //     if (smallest > arr[i]) {
-
-  //     }
-  //   }
-  // }
-
-  // if (queue.peek() !== undefined) {
-  //   let smallest = queue.dequeue();
-  //   const queue2 = new Queue();
-  //   // console.log(queue.print());
-  //   let count = 0;
-  //   while (queue.peek() !== undefined) {
-  //     count++;
-  //     queue2.enqueue(queue.dequeue());
-  //   }
-
-  //   for (let i = 0; i < count; i++) {
-  //     if (smallest > queue2.peek()) {
-  //       queue.enqueue(smallest);
-  //       smallest = queue2.dequeue();
-  //     } else {
-  //       queue.enqueue(queue2.dequeue());
-
-  //     }
-  //   }
-  //   console.log(smallest);
-  //   console.log('');
-  //   return smallest;
-
-  // }
 
   if (queue.peek() !== undefined) {
     let successorGreater = true;
