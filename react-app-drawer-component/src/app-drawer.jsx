9a74694abc3a67d2b8fb08ccf2aabd73f3ac5f57
rskidmore1 <strictlybusinessbythebook@yahoo.com>
2021-09-22 03:34:33 +0000

    Refactored two html sections into one

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -16,51 +16,24 @@ class Drawer extends React.Component {
   }
 
   render() {
-    let button;
-    if (this.state.isOpen) {
 
-      button = <div>
-      <div className="">
-
+    return (
+      <React.Fragment>
+        <div className = "" >
           <i className="fas fa-bars" onClick={this.toggle}></i>
-
         </div>
-        <div className="nav-div on-screen">
+        <div className={this.state.isOpen ? 'nav-div on-screen' : 'nav-div off-screen'} >
           <h2 onClick={this.toggle}>Menu</h2>
           <div>
-          <ul>
+            <ul>
               <li><p onClick={this.toggle}>About</p></li>
               <li><p onClick={this.toggle}>Get Started</p></li>
               <li><p onClick={this.toggle}>Sign In</p></li>
-          </ul>
+            </ul>
 
-         </div>
-      </div>
-      </div>;
-    } else if (!this.state.isOpen) {
-      button = <div>
-       <div>
-
-        <i className="fas fa-bars" onClick={this.toggle}></i>
-
-      </div>
-        <div className="nav-div off-screen" >
-          <h2 onClick={this.toggle}>Menu</h2>
-        <div>
-          <ul>
-            <li><p onClick={this.toggle}>About</p></li>
-            <li><p onClick={this.toggle}>Get Started</p></li>
-            <li><p onClick={this.toggle}>Sign In</p></li>
-          </ul>
-
-         </div>;
-
+          </div>
         </div>
-      </div>;
-
-    }
-    return (
-      <div>{button}</div>
+      </React.Fragment>
     );
   }
 
